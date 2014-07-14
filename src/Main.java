035edcd9f735ac17f19628fb5cbc749ade180649
ioannis kostopoulos <ioannkostopoulos@gmail.com>
2014-07-14 12:41:51 +0300

    θηυθδηφασδ

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Toy example");
+		System.out.println("Toy example")
+  ;
+		System.out.println("Toy 2");
 
 	}
 
